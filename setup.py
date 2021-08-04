313b806af19633135527585e764ea098d41c90d1
Radhika Gupta <radhikagupta1378@gmail.com>
2021-08-04 14:40:41 +0100

    feature: make src folder installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Radhika Gupta",
+    author_email="radhikagupta1378@gmail.com",
+    description="Biofilm project package",
+    url="url-to-github-page",
+    packages=find_packages(),
+)
