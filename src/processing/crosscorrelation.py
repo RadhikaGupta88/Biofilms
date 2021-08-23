b20edeed6957a924125fbae1488e0ed751712870
Radhika Gupta <radhikagupta1378@gmail.com>
2021-08-23 10:46:01 +0100

    cleanup: organise code into functions

--- src/processing/crosscorrelation.py
+++ src/processing/crosscorrelation.py
@@ -7,11 +7,15 @@ from src.processing.radial import create_radius_select_stack
 
 def crosscorrelation_0_to_j(image_stack, start_slice, stop_slice, x_centres, y_centres, plot=False):
     """
-    Calculates the crosscorrelation between one slice and a given number of adjacent slices and plots
+    Calculates the crosscorrelation between one slice and a given number of adjacent slices within a
+    given stack, created using the 'create_radius_select_stack' function, and plots.
+
+    x_centres and y_centres are 1D arrays/lists of the (x,y) centre coords for each timestamp.
     """
     image_array = image_stack[start_slice:stop_slice]
     x_centre_array, y_centre_array = x_centres[start_slice:stop_slice], y_centres[start_slice:stop_slice]
     x_centre, y_centre = np.mean(x_centre_array), np.mean(y_centre_array)
+
     stack_list = []
     for i in range(len(image_array)):
         coord_stack = create_radius_select_stack(image_array[i], (x_centre, y_centre), 300, 0.5)
@@ -28,5 +32,5 @@ def crosscorrelation_0_to_j(image_stack, start_slice, stop_slice, x_centres, y_c
     if plot==True:
         plt.figure(figsize = [20,10])
         plt.plot(slice_separation, crosscorrelation_list)
-    
+
     return slice_separation, crosscorrelation_list
--- src/processing/fourier_spectrum.py
+++ src/processing/fourier_spectrum.py
@@ -1,63 +1,66 @@
-from skimage.io import imread
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
-#
-# stack_b = imread('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/biolight_gb.tif')
-# stack_n = imread('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/normallight_edge_gb.tif')
-
-stack_b = imread('c:/Cambridge/Mechanics of biofilm/original videos/200920_biolight_orginal.tif')
-stack_n = imread('c:/Cambridge/Mechanics of biofilm/original videos/200920_normallight_orginal.tif')
-
-def slice_from_stack(stack, slice):
-    image = stack[slice]
-    return image
 
 def fft_image(image):
+    """
+    Finds and returns the fourier spectrum of an image
+    """
     image = np.fft.fftshift(np.fft.fft2(image))
     image = np.log(abs(image))
     return image
 
 
-img_b = slice_from_stack(stack_b, 10)
-img_n = slice_from_stack(stack_n, 10)
-img_fft_b = fft_image(img_b)
-img_fft_n = fft_image(img_n)
+def plot_fourier_spectrum_subplots(stack_b, stack_n):
+    """
+    Creates an interactive set of 4 subplots, two of which are images from given biolight
+    and brightfield stacks, and the other two are their fourier spectrums
+    """
 
-# set up figure
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
-plt.subplots_adjust(left = 0.1, bottom = 0.35)
+    slice_no_b = 10
+    STACK_B_TO_N_RATIO = len(stack_b) / len(stack_n)
+    slice_no_n = int(slice_no_b / STACK_B_TO_N_RATIO)
 
-# create slider
-axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
-slice_slider = Slider(axSlider, 'stack slice', valmin=1, valmax=322, valstep=1)
-
-plot_b = ax1.imshow(img_b)
-plot_n = ax2.imshow(img_n)
-
-plot_fft_b = ax3.imshow(img_fft_b)
-plot_fft_n = ax4.imshow(img_fft_n)
-
-
-def val_update(val):
-    slice = slice_slider.val - 1
-
-    img_b = slice_from_stack(stack_b, slice)
-    img_n = slice_from_stack(stack_n, slice)
-
+    img_b = stack_b[slice_no_b]
+    img_n = stack_n[slice_no_n]
     img_fft_b = fft_image(img_b)
     img_fft_n = fft_image(img_n)
 
-    plot_b.set_data(img_b)
-    plot_n.set_data(img_n)
+    # set up figure
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
+    plt.subplots_adjust(left = 0.1, bottom = 0.35)
 
-    plot_fft_b.set_data(img_fft_b)
-    plot_fft_n.set_data(img_fft_n)
+    # create slider
+    axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
+    slice_slider = Slider(axSlider, 'stack slice', valmin=1, valmax=322, valstep=1)
 
-    plt.draw()
-    #plt.show()
+    plot_b = ax1.imshow(img_b, cmap='gray')
+    plot_n = ax2.imshow(img_n, cmap='gray')
+
+    plot_fft_b = ax3.imshow(img_fft_b, cmap='gray')
+    plot_fft_n = ax4.imshow(img_fft_n, cmap='gray')
 
 
-slice_slider.on_changed(val_update)
+    def val_update(val):
+        slice = slice_slider.val - 1
 
-plt.show()
+        img_b = stack_b[slice]
+        img_n = stack_n[int(slice / STACK_B_TO_N_RATIO)]
+
+        img_fft_b = fft_image(img_b)
+        img_fft_n = fft_image(img_n)
+
+        plot_b.set_data(img_b)
+        plot_n.set_data(img_n)
+
+        plot_fft_b.set_data(img_fft_b)
+        plot_fft_n.set_data(img_fft_n)
+
+        plt.draw()
+        #plt.show()
+
+
+    slice_slider.on_changed(val_update)
+
+    plt.show()
