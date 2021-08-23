ee058f540743d59ad1947fe6e7185f2eb13ead89
Ricky Li <rickyli@Rickys-MacBook-Pro.local>
2021-08-23 12:05:11 +0100

    Removed intensity profile, moved new script into src
    Found new location for intensity profile scripts in src, so removed duplicates

--- src/processing/intensity_profile_subplots.py
+++ src/processing/intensity_profile_subplots.py
@@ -1,82 +1,81 @@
-import matplotlib.image as mpimg
+###
+# intensity_profile_subplots.py
+#
+# This script takes in two images of the same biofilm, but one image bl_img
+# is taken without a brightfield and shows the bioluminescence of the biofilm,
+# while bf_img is taken with a brightfield.
+#
+# It returns an interactive matplotlib image which picks out a row of the image
+# and plots the light intensity along the row for both images on one subplot,
+# while showing the row being plotted on an image of the bioluminescent image.
+# A slider allows the row to be selected by user.
+###
+
+"""
+Dependencies
+"""
+from src.processing.constants import BUCKLING_PATH, MECHANICS_OF_BIOFILM_PATH
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Button, Slider
 from scipy.ndimage.filters import gaussian_filter
 from skimage.io import imread
 
+"""
+Constants
+"""
+path_to_bl_img = 'c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/200920_biolight_nobg_nooutliers_despeckle_enhance.tif'
+path_to_bm_img = 'c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/200920_normallight_nobg_edges_gaussblur_enhance.tif'
 
-img1 = imread('test_biolum.jpg')
-img2 = imread('test_bright.jpg')
+"""
+Functions
+"""
+def val_update(val):
+    """
+    Updates the row we are looking at upon changes of the slider position
+    :param val: Value passed from Slider widget, giving the current row
+    """
+    row = int(row_slider.val) # row_slider.val is a float, so needs converting
+    bio_plot.set_ydata(bl_img[row])
+    bright_plot.set_ydata(bf_img[row])
+    line.set_ydata([row]*columns)
+    plt.draw()
 
-img3 = imread('adjusted biolum.jpg')
-img3 = gaussian_filter(img3, sigma=1)
-rows3, columns3 = np.shape(img3)
+"""
+Script
+"""
 
-img4 = imread('adjusted brightfield.jpg')
-img4 = gaussian_filter(img4, sigma=1)
+# Read in images for bioluminescent and brightfield, and apply gaussian blur
+bl_img = 0.5*imread(path_to_bl_img)[150]
+bl_img = gaussian_filter(bl_img, sigma=7)
 
+bf_img = 0.9*imread(path_to_bm_img)[150]
+bf_img = gaussian_filter(bf_img, sigma=7)
 
-img3 = 0.5*imread('c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/200920_biolight_nobg_nooutliers_despeckle_enhance.tif')[150]
-img4 = 0.9*imread('c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/200920_normallight_nobg_edges_gaussblur_enhance.tif')[150]
-img3 = gaussian_filter(img3, sigma=1)
-img4 = gaussian_filter(img4, sigma=1)
-def intensity_profile_interactive(biolum,bright,row,low=0, high=1388):
+# Read in parameters of the image. Both are 1388x1040
+rows, columns = np.shape(bl_img)
 
-    if len(np.shape(biolum)) == 3:
-        bio_data = biolum[row, :, 0]
-        bright_data = bright[row, :, 0]
-        integers = list(range(1, len(bio_data)+1))
-
-    if len(np.shape(biolum)) == 2:
-        bio_data = biolum[row, :]
-        bright_data = bright[row, :]
-        integers = list(range(1, len(bio_data) + 1))
-
-    return integers, bio_data, bright_data
-
- # fig = px.line(integers[low:high], bio_data[low:high])
-    # fig.show()
-
-
+# Create axes
 fig, (ax1, ax2) = plt.subplots(1,2)
 plt.subplots_adjust(left = 0.1, bottom = 0.35)
 
-#subplot 1
-integers, bio_data, bright_data = intensity_profile_interactive(img3, img4, 600)
-bio_plot, = ax1.plot(integers, bio_data, label='bioluminescence')
-bright_plot, = ax1.plot(integers, bright_data, label='brightfield')
-ax1.legend(loc="upper right")
+# Subplot 1
+initial_row = 500
+bio_plot, = ax1.plot(bl_img[initial_row], label='bioluminescence')
+bright_plot, = ax1.plot(bf_img[initial_row], label='brightfield')
 ax1.set_xlabel('Column')
 ax1.set_ylabel('Pixel intensity')
 ax1.set_title('Intensity profile')
-# ax1.xlabel('Column')
-# ax1.ylabel('Pixel intensity')
-#plt.legend()
+ax1.legend()
 
-axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
-row_slider = Slider(axSlider, 'row', valmin=1, valmax=1039, valstep=1)
-
-#subplot 2
-ax2.imshow(img4)
-line, = ax2.plot(np.zeros(columns3), color='r', linestyle='-')
+# Subplot 2
+ax2.imshow(bf_img)
+line, = ax2.plot([initial_row]*columns, color='r', linestyle='-')
 ax2.set_title('Brightfield image')
 
-def val_update(val):
-    row = row_slider.val
-    bio_plot.set_ydata(intensity_profile_interactive(img3, img4, row)[1])
-    bright_plot.set_ydata(intensity_profile_interactive(img3, img4, row)[2])
-    line.set_ydata(row)
-    plt.draw()
-
+# Slider
+axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
+row_slider = Slider(axSlider, 'row', valmin=1, valmax=1039, valstep=1)
 row_slider.on_changed(val_update)
 
-#plt.title('Intensity profile',  y=14)
 plt.show()
-
-#intensity_profile(img1,img2,800)
-#intensity_profile(img3,img4,700)
-#intensity_profile_interactive(img3,img4,700)
-
-# axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
-# row_slider = Slider(axSlider, 'row', valmin=1, valmax=1040)
