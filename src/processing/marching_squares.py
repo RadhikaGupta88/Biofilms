4598187c64b52dc062e7688586a4d2a36649c95a
Radhika Gupta <radhikagupta1378@gmail.com>
2021-08-23 15:23:18 +0100

    cleanup: marching squares notebook

--- src/processing/marching_squares.py
+++ src/processing/marching_squares.py
@@ -1,22 +1,9 @@
-
-import imageio
 import matplotlib.pyplot as plt
 import numpy as np
-import tifffile
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-from PIL import Image
-from skimage import measure
+from skimage import measure, color
 from skimage.io import imread
 from tifffile import imwrite
-
-#img1 = imread('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/200920_biolight_marching_edges.tif')[150]
-#img1 = imread('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/200920_biolight_marching_binary.tif')
-#img1 = imread('c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/buckling/200920_normallight_bw_imagej.tif')[int(130*1.403726708)]
-img1 = imread('c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/buckling/200920_normallight_nobg_edges_gaussblur_enhance_inverted.tif')[int(130*1.403726708)]
-#img1 = imread('c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/buckling/200920_biolight_nobg_nooutliers_despeckle_enhance.tif')[130]
-img_og = imread('c:/Cambridge/Mechanics_of_biofilm/algorithm for clear images/buckling/200920_biolight_nobg_nooutliers_despeckle_enhance.tif')[130]
-#img_og = imread('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/200920_normallight_bw_imagej.tif')[200]
 
 
 def marching_squares_contours(image, threshold = 50, max_length = 20):
@@ -37,15 +24,13 @@ def marching_squares_contours(image, threshold = 50, max_length = 20):
 
 def plot_contours(contours, original_image):
     fig, ax1 = plt.subplots()
+    fig.set_size_inches(10,10)
     ax1.imshow(original_image, cmap='Greens')
     for line in contours:
         ax1.plot(line[:, 1], line[:, 0], linewidth=1, color='r')
-     #ax1.invert_yaxis()
+     #ax1.invert_yaxis() #for when not plotting the image as well
     ax1.set(aspect=1)
     fig.canvas.draw()
-
-    #width, height = fig.get_size_inches() * fig.get_dpi()
-    #mplimage = np.fromstring(fig.canvas.tostring_rgb(), dtype='uint8').reshape(height, width, 3)
 
     data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     w, h = fig.canvas.get_width_height()
@@ -55,17 +40,13 @@ def plot_contours(contours, original_image):
 
     #fig = np.array(fig)
     #plt.savefig('marching squares stack/image_from_contours_'+str(slice_number)+'.tif', bbox_inches='tight', pad_inches=0)
-    #print(type(fig))
-    #plt.show()
-    #print(type(gray_image))
+
     plt.show()
     return gray_image
 
 
-# fig, ax1 = plt.subplots()
-# ax1.imshow(img_og, cmap=plt.cm.gray)
-# #ax2.imshow(img1, cmap=plt.cm.gray)
 
+#not yet fully implemented...
 def full_stack_contours(stack_name, original_image_stack):
     images = []
     stack = imread(stack_name)
@@ -85,37 +66,3 @@ def full_stack_contours(stack_name, original_image_stack):
     #io.imsave("full stack.png", img_as_uint(images))
     #plt.imsave('full_stack.jpeg', images)
     return images
-
-contours = marching_squares_contours(img1, 16, 20)
-plot_contours(contours, img_og)
-
-#plt.axis('off')
-#fig.axes.get_xaxis().set_visible(False)
-#fig.axes.get_yaxis().set_visible(False)
-#plt.savefig('image_from_contours.png', bbox_inches='tight', pad_inches = 0)
-
-def fft_image(image):
-    image = np.fft.fftshift(np.fft.fft2(image))
-    image = np.log(abs(image))
-    return image
-
-
-# images = full_stack_contours('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/200920_biolight_marching_binary.tif',
-#                     'c:/Cambridge/Mechanics of biofilm/algorithm for clear images/200920_biolight_nobg_nooutliers_despeckle_enhance.tif')
-#
-# rows, columns, slices = np.shape(images)
-# images = np.reshape(images, (slices, rows, columns))
-#
-# #tifffile.imsave('test.tif', images)
-# abs = tifffile.imwrite('test.tif', images)
-# print(abs)
-#
-# imgtiff = imread('test.tif')
-# print(np.shape(images))
-# print(np.shape(imgtiff))
-# # print(img)
-# # plt.imshow(img)
-# # plt.show()
-#
-# img = imread('c:/Cambridge/Mechanics of biofilm/algorithm for clear images/200920_biolight_marching_binary.tif')
-# print(np.shape(img))
